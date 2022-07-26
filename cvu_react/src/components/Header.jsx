13037a920aae8b6d9913739f656d393d5893c9a9
Ian Hansson <ian713hansson@gmail.com>
2022-07-26 11:40:15 -0500

    starting to style

--- cvu_react/src/components/Header.jsx
+++ cvu_react/src/components/Header.jsx
@@ -9,15 +9,13 @@ const Header = () => {
             <h1>Celestial Vibration Unit</h1>
             <h3>...a modular synthesizer living in a virtual world</h3>
         </div>
-      <div className='nav'>
-      <nav>
+      <nav className='nav'>
         <Link to="/">Home</Link>
         <Link to="/about">About Me</Link>
         <Link to="/explore">Explore</Link>
         <Link to="/listen">Listen</Link>
         <Link to="/member">Become a Member</Link>
       </nav>
-      </div>
     </header>
   )
 }
