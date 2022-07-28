9e3471e66362904d10e3296fb8dcb7064dc09456
Ian Hansson <ian713hansson@gmail.com>
2022-07-28 16:48:51 -0500

    heroku

--- db/index.js
+++ db/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/modulesDatabase'
+require('dotenv').config()
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/modulesDatabase'
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
