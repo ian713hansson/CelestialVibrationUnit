5a3836b485477af61e378d59501f89fb9a6b2ec1
Ian Hansson <ian713hansson@gmail.com>
2022-08-23 11:30:16 -0500

    uploading to heroku

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.json())
 
 app.use(logger('dev'))
 
-app.use(express.static(`${__dirname}/client/build`))
-
 app.use('/api', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection failed...'))
+
+app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/client/build/index.html`)
